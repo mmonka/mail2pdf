ad608e7dc2ce1b296aadc9d6832c5a1f1279e750
Markus Monka <mmonka@gmail.com>
2015-01-25 13:07:40 +0100

    Adding more MIME Body handling

--- mbox2pdf.pl
+++ mbox2pdf.pl
@@ -1,69 +1,194 @@
 #!/usr/bin/perl
 
+use lib '/Users/markus/perl5/lib/perl5/';
+
 use strict;
 use warnings;
 use Data::Dumper;
 use Mail::Mbox::MessageParser;
+use MIME::Parser;
+use MIME::Words qw(:all);
+use MIME::Body;
 use Email::Simple;
+use Email::MIME;
 use Getopt::Long;
-    
+   
+# --------------------------------------------------
 # Global Variables
+# --------------------------------------------------
 my $mboxfile;
 my $verbose;
 my $debug;
 
+# --------------------------------------------------
+# Getopt definition
+# --------------------------------------------------
 GetOptions(	"mboxfile=s" => \$mboxfile, # string
     		"verbose" => \$verbose,
     		"debug" => \$debug
 	  ) # flag
 or die("Error in command line arguments\n");
 
+ 
+MIME::Tools->debugging(1) if($debug);
+MIME::Tools->quiet(1) if($verbose);
+
+# --------------------------------------------------
 # Check file
+# --------------------------------------------------
 if (!check_mbox_file($mboxfile)) {
 
 	error("FATAL", "File '$mboxfile' does not fit");
 	exit;
 }
 
+# --------------------------------------------------
+# new FileObjekt
+# --------------------------------------------------
 my $filehandle = new FileHandle($mboxfile);
 
 
 # Set up cache
-Mail::Mbox::MessageParser::SETUP_CACHE(	{ 'file_name' => '/tmp/cache' } );
+# Mail::Mbox::MessageParser::SETUP_CACHE(	{ 'file_name' => '/tmp/cache' } );
 
-# Vars
-my $folder_reader = new Mail::Mbox::MessageParser( {
+# --------------------------------------------------
+# new MboxParser Objekt
+# --------------------------------------------------
+my $mbox = new Mail::Mbox::MessageParser( {
 		'file_name' => $mboxfile,
 		'file_handle' => $filehandle,
-		'enable_cache' => 1,
-		'enable_grep' => 1,
+		'enable_cache' => 0,
+		'enable_grep' => 0,
 		'debug' => $debug,
 		} );
 
 
 
-die $folder_reader unless ref $folder_reader;
+die $mbox unless ref $mbox;
 
-warn "No cached information" if $Mail::Mbox::MessageParser::Cache::UPDATING_CACHE;
-
+# --------------------------------------------------
 # Any newlines or such before the start of the first email
-my $prologue = $folder_reader->prologue;
+# --------------------------------------------------
+my $prologue 	= $mbox->prologue;
 
+# --------------------------------------------------
+# value for logging
+# --------------------------------------------------
+my $email_count = 1;
+
+# --------------------------------------------------
 # This is the main loop. It's executed once for each email
-while(! $folder_reader->end_of_file() )
+# --------------------------------------------------
+while(! $mbox->end_of_file() )
 {
-	my $content = $folder_reader->read_next_email();
-	my $email = Email::Simple->new($content);
+	my $content = $mbox->read_next_email();
 	
-	# Date, Subject
-	my $date = $email->header("Date");
-	my $subject = $email->header("Subject");
+	my $parser = new MIME::Parser;
 
-	my $body = $email->body;
+	$parser->ignore_errors(0);
+	$parser->output_to_core(5);
 
-	logging("Info", "Subject '$subject'\n Date '$date'");
+	my $entity = $parser->parse_data($content);
+	my $error = ($@ || $parser->last_error);
 	
+	# get email headers
+	my $header = $entity->head;
+	my $subject = $header->get('Subject');
+	my $to = $header->get('To');
+	my $from = $header->get('From');
+	my $date = $header->get('Date');
+	my $contenttype = $header->get("Content-Type");
+
+	# if from facebook, skip
+	next if($from =~ /facebook/);
+
+	# delete newlines
+	chomp($subject);
+	chomp($to);
+	chomp($from);
+	chomp($date);
+	chomp($contenttype);
+
+	# decode subject 
+	if($subject =~ /.*(utf-8|utf8).*/) {
+
+		logging("DEBUG", "Subject encoding is utf8");
+		my $decoded = decode_mimewords($subject);
+
+		# Fix encoding
+		$subject = $decoded;
+
+	}
+	
+	logging("INFO", "Email Nr: $email_count");
+	logging("INFO", "Date '$date'");
+	logging("INFO", "From '$from'");
+	logging("INFO", "Subject '$subject'");
+	logging("INFO", "Email Content-Type '$contenttype'\n");
+	
+	# returns a hash ref with mime content
+	my $mail = handle_mime_body($entity);
+
+	$email_count++;	
 }
+
+# --------------------------------------------------------
+# Handle Body  
+# --------------------------------------------------------
+sub handle_mime_body {
+
+	my $entity 	= shift;
+
+	my $plain_body 	= "";
+	my $html_body 	= "";
+	my $content_type;
+
+	# --------------------------------------------
+	# get email body
+	# --------------------------------------------
+	if ($entity->parts > 0){
+
+    		for (my $i=0; $i<$entity->parts; $i++){
+
+			# Mime Parts 
+			my $subentity = $entity->parts($i);
+
+			# For "singlepart" types (text/*, image/*, etc.), the unencoded body data is referenced 
+			# via a MIME::Body object, accessed via the bodyhandle() method
+			if($subentity->head->get('content-type') =~ "text") {
+				
+				my $body	= $subentity->bodyhandle;
+				my @lines 	= $body->as_lines;
+				my $string	= "";				
+
+				foreach (@lines) {
+
+					chomp($_);
+					$string = sprintf("%s %s", $string, $_);
+
+				}
+				
+				logging("VERBOSE", "Part $i - Type Text '$string'");
+			}
+			elsif($subentity->head->get('content-type') =~ "image") {
+
+				logging("VERBOSE", "Part $i - Type Image");
+			}
+			elsif($subentity->head->get('content-type') =~ "video") {
+
+				logging("VERBOSE", "Part $i - Type Video");
+			}
+		}
+	}
+	else {
+
+		logging("INFO", "No Body-Part found");
+	}
+
+	return 0;
+}
+
+
 # --------------------------------------------------------
 # Logging 
 # --------------------------------------------------------
@@ -71,7 +196,19 @@ sub logging {
 
 	my ($LEVEL, $msg) = @_;
 
-	printf("%s\n", $msg);
+	if($debug && $LEVEL eq "DEBUG") {
+
+		printf("%s: %s\n", $LEVEL, $msg);
+	} 
+	elsif ($verbose && ( $LEVEL eq "VERBOSE" || $LEVEL eq "INFO" ) )  {
+
+		printf("%s: %s\n", $LEVEL, $msg);
+	} 
+	else {
+
+		# no logging
+	}
+
 	return 0;
 }
 
